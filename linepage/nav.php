ab08aca42590ebc98649418f393a252e26ed3d3f
snoluv45 <pasitza115@gmail.com>
2024-03-14 20:03:17 +0700

    line

--- linepage/nav.php
+++ linepage/nav.php
@@ -1,8 +1,9 @@
+
 <div class="container">
-<header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-        LINE LOGIN WEBSITE
-      </a>
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        Line Login
+    </a>
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
@@ -13,15 +14,15 @@
       </ul>
 
       <div class="col-md-3 text-end">
-        <?php
+        <?php 
             if (!isset($_SESSION['profile'])) {
                 $line = new LineLogin();
                 $link = $line->getLink();
         ?>
-        <a href="<? echo $link; ?>" class="btn btn-outline-primary me-2">Login</a>
-        <?php } else { ?> 
-        <a href="logout.php" class="btn btn-danger">Logout</a>
+        <a href="<?php echo $link; ?>" class="btn btn-success me-2">Line Login</a>
+        <?php } else { ?>
+            <a href="logout.php" class="btn btn-danger">Logout</a>
         <?php } ?>
-      </div>
+        </div>
     </header>
-    </div>
+  </div>
